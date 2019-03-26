dfa073c3e55fa865016f490d4321fc13b366b49a
Seil Cho <seilcho7@hotmail.com>
2019-03-26 13:58:55 -0400

    adds test for users.js

--- models/conn.js
+++ models/conn.js
@@ -1,6 +1,13 @@
 // first require 'pg-promise'
 // call it immediately, which gives us a configured database connector
-const pgp = require('pg-promise')();
+const pgp = require('pg-promise')({
+    query: e => {
+        console.log('QUERY: ', e.query);
+    }  
+});
+
+
+
 const options = {
     host: 'localhost',
     database: 'restaurants-app'
--- test/test.js
+++ test/test.js
@@ -1,7 +1,55 @@
-const assert = require('assert');
+// const assert = require('assert');
+const chai = require('chai');
+const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised).should();
 
-describe('Sanity check', function () {
-    it('should be 2', function () {
-        assert.equal(2, 1 + 1);
+const User = require('../models/users');
+const Restaurant = require('../models/restaurants');
+
+// add a "describe block" for restaurant tests
+describe('Restaurant model', () => {
+    it('should be able to grab an array of restaurants', async () => {
+        // write the code you wish existed
+        const arrayOfRestaurants = await Restaurant.getAll();
+        expect(arrayOfRestaurants).to.be.instanceOf(Array);
+    })
+});
+
+
+// describe('Sanity check', function () {
+//     it('should be 2', function () {
+//         // assert.equal(2, 1 + 1);
+//         expect(1 + 1).to.equal(2);
+//     });
+// });
+
+describe('Users model', () => {
+    // Happy path 😃
+    it('should be able to retreive by id', async () => {
+        const theUser = await User.getById(3);
+        expect(theUser).to.be.instanceOf(User);
+        // theUser.should.be.an.instanceOf(User);
+        // theUser.should.have.length(1);
+    });
+    // Sad path 😢
+    it('should error if no user by id', async () => {
+        const theUser = await User.getById(3242);
+        expect(theUser).to.be.null;
+        // theUser.should.be.an.instanceOf(User);
+        // theUser.should.have.length(1);
+    });
+
+    it('should update the user', async () => {
+        // grab a user with id 2
+        const theUser = await User.getById(2);
+        // update the email
+        theUser.email = "new@new.com";
+        // save the user
+        theUser.save();
+        // re-grab the user with id 2
+        const alsoTheUser = await User.getById(2);
+        // expect the email to be equal to the new value
+        expect(alsoTheUser.email).to.equal('new@new.com');
     });
 });
